50d43cc27c34c5179c970428fb9cac02dc12246d
GabrielGameDev <gabrielgamedev@gmail.com>
2025-04-05 22:47:15 -0300

    Dash

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,32 +14,38 @@ public class PlayerController : MonoBehaviour
     public Vector2 screenLimit;
 	private Rigidbody2D rb; 
     private bool isGrounded;
-	private bool canDoubleJump;
+	private bool hasDoubleJump;
+	private bool hasDash;
+	public float dashForce = 10;            	
+	public float dashDuration = 0.25f;
+	bool isDashing;
+	float dashTime;
+	bool canDash;
 	bool usedDoubleJump;
 	AudioSource audioSource;
 	float targetVelocityX = 0f;
 	public bool jumpPressed;
 
 	[Header("Wall")]
-	public bool onWall;
-	public Vector3 wallOffset;
-	public float wallRadius;
-	public float maxFallSpeed = -1;
-	public float wallJumpDuration = 0.25f;
-	public bool jumpFromWall;
-	public float jumpFinish;
+	public bool onWall;	
+	public float maxFallSpeed = -1;	
 	public LayerMask wallLayer;
-	bool canMove = true;
 	public float horizontalJumpForce = 6;
-	public int direction = 1;
+	public float wallJumpTime = 0.5f;
+	public float wallCheckDistance = 0.2f;
+	int direction = 1; // 1 for right, -1 for left
+	Animator animator;
 
-	public bool CanDoubleJump { get => canDoubleJump; set => canDoubleJump = value; }
+
+	public bool HasDoubleJump { get => hasDoubleJump; set => hasDoubleJump = value; }
+	public bool HasDash { get => hasDash; set => hasDash = value; }
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 		audioSource = GetComponent<AudioSource>();
+		animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -49,35 +56,9 @@ public class PlayerController : MonoBehaviour
         RaycastHit2D leftCheck = Raycast(new Vector2(-footOffset, 0), Vector2.down, groundCheckDistance, groundLayer);
         isGrounded = rightCheck || leftCheck;
         
-        if(Input.GetButtonDown("Jump") && (isGrounded))
-        {
-			audioSource.PlayOneShot(jumpSounds[Random.Range(0, jumpSounds.Length)]);			
-			rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            usedDoubleJump = false;
-		}
-        else if(Input.GetButtonDown("Jump") && (canDoubleJump && !usedDoubleJump))
-        {
-			audioSource.PlayOneShot(jumpSounds[Random.Range(0, jumpSounds.Length)]);
-			usedDoubleJump = true;            
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);			
-		}
-		else if (jumpPressed && onWall && !isGrounded)
-		{
-			canMove = false;
-			jumpFinish = Time.time + wallJumpDuration;
-			jumpPressed = false;
-			jumpFromWall = true;
-			//Flip();
-
-			rb.linearVelocity = Vector2.zero;
-
-			rb.AddForce(new Vector2(horizontalJumpForce * direction, jumpForce), ForceMode2D.Impulse);
-		}
-		if (Input.GetButtonUp("Jump"))
-        {
-			rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-		}
+		if((isGrounded || onWall) && Time.time > dashTime)
+			canDash = true;
+    
 		float moveInput = Input.GetAxis("Horizontal");
 
 		// Deadzone para joystick
@@ -91,28 +72,119 @@ public class PlayerController : MonoBehaviour
 		transform.position = pos;
 
 		//check wall
-		bool rightWall = Physics2D.OverlapCircle(transform.position + new Vector3(wallOffset.x, 0), wallRadius, wallLayer);
-		bool leftWall = Physics2D.OverlapCircle(transform.position + new Vector3(-wallOffset.x, 0), wallRadius, wallLayer);
-		if (rightWall || leftWall)
-		{
-			onWall = true;
-		}
+		bool onWallCheck = Raycast(Vector2.zero, Vector2.right * direction, wallCheckDistance, wallLayer);		
 
+		if(onWallCheck)
+		{
+			onWall = true;			
+		}
+		
 		if (onWall)
 		{
+			
 			if (rb.linearVelocity.y < maxFallSpeed)
 			{
 				rb.linearVelocity = new Vector2(rb.linearVelocity.x, maxFallSpeed);
 			}
 		}
+		
+
+		if (onWall)
+			return;
+		if (moveInput > 0 && direction < 0)
+		{
+			Flip();
+		}
+		else if (moveInput < 0 && direction > 0)
+		{
+			Flip();
+		}
+
+		
+
+	}
+
+	public void Dash(InputAction.CallbackContext ctx)
+	{
+		if(ctx.performed)
+		{
+			if (canDash)
+			{
+				isDashing = true;
+				dashTime = Time.time + dashDuration;
+				canDash = false;
+				animator.SetTrigger("dash");
+				audioSource.PlayOneShot(jumpSounds[Random.Range(0, jumpSounds.Length)]);
+				rb.gravityScale = 0;
+				transform.position += Vector3.up * 0.25f;
+				rb.linearVelocity = Vector2.zero;          //zera a velocidade do jogador antes de executar o dash
+				rb.AddForce(Vector2.right * direction * dashForce, ForceMode2D.Impulse); //adiciona força do dash
+				Invoke("ReleaseDash", dashDuration);
+			}
+		}
+	}
+
+	void ReleaseDash()
+	{
+		isDashing = false;
+		rb.gravityScale = 1;
+	}
+
+	public void Jump(InputAction.CallbackContext ctx)
+	{
+		if(ctx.performed)
+		{
+			if (isGrounded)
+			{				
+				animator.SetTrigger("jump");
+				audioSource.PlayOneShot(jumpSounds[Random.Range(0, jumpSounds.Length)]);			
+				rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+				usedDoubleJump = false;
+			}
+			else if (hasDoubleJump && !usedDoubleJump)
+			{
+				usedDoubleJump = true;
+				rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+				animator.SetTrigger("jump");
+				audioSource.PlayOneShot(jumpSounds[Random.Range(0, jumpSounds.Length)]);
+				rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+			}
+			else if(onWall && !isGrounded)
+			{
+				animator.SetTrigger("jump");
+				audioSource.PlayOneShot(jumpSounds[Random.Range(0, jumpSounds.Length)]);
+				rb.AddForce(new Vector2(horizontalJumpForce * -direction, jumpForce), ForceMode2D.Impulse);
+				Invoke("ResetOnWall", wallJumpTime);
+			}
+			
+		}
+		else if(ctx.canceled)
+		{
+			rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+		}
+	}
+
+	void Flip()
+	{
+		direction *= -1;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 
 	private void FixedUpdate()
 	{
-		float newVelocityX = Mathf.Lerp(rb.linearVelocity.x, targetVelocityX, stoppingSpeed * Time.fixedDeltaTime);
-		rb.linearVelocity = new Vector2(newVelocityX, rb.linearVelocity.y);
+		if (onWall || isDashing)
+			return;
 
-		
+		float newVelocityX = Mathf.Lerp(rb.linearVelocity.x, targetVelocityX, stoppingSpeed * Time.fixedDeltaTime);
+		rb.linearVelocity = new Vector2(newVelocityX, rb.linearVelocity.y);		
+		animator.SetFloat("speed", Mathf.Abs(newVelocityX));
+	}
+
+	void ResetOnWall()
+	{
+		onWall = false;
 	}
 
 	RaycastHit2D Raycast(Vector2 offset, Vector2 rayDirection, float length, LayerMask mask)
@@ -127,4 +199,6 @@ public class PlayerController : MonoBehaviour
 
 		return hit;
 	}
+
+	
 }
