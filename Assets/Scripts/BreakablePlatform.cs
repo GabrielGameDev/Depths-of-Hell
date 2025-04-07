6f13972c03e7ab277dc702d4b69e4e81a9604eb4
GabrielGameDev <gabrielgamedev@gmail.com>
2025-04-07 18:57:29 -0300

    Som da plataforma

--- Assets/Scripts/BreakablePlatform.cs
+++ Assets/Scripts/BreakablePlatform.cs
@@ -4,8 +4,12 @@ public class BreakablePlatform : MonoBehaviour
 {
 	public float destroyDelay = 0.5f; // Delay before the platform is destroyed
 	public Animator animator; // Reference to the Animator component
+	AudioSource audioSource;
 
-	
+	private void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
 
 
 	private void OnTriggerEnter2D(Collider2D collision)
@@ -15,9 +19,11 @@ public class BreakablePlatform : MonoBehaviour
 			Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
             if (playerRb.linearVelocity.y <= 0)
             {
+				audioSource.Play(); // Play rumble sound
 				GetComponent<Collider2D>().enabled = false; // Disable the collider to prevent further collisions
 				animator.enabled = true; // Enable the animator to play the animation
 				Destroy(transform.parent.gameObject, destroyDelay); // Destroy the platform after the delay
+				
 			}
             
 		}
